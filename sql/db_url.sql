bb6cfde4407bf5bbdd4133217e4587187875a1f8
RileDev <rileristic115@gmail.com>
2023-10-10 21:28:37 +0200

    jhgjghj

--- sql/db_url.sql
+++ sql/db_url.sql
@@ -35,28 +35,6 @@ CREATE TABLE `sites` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `sites`
---
-
-INSERT INTO `sites` (`id`, `code`, `url`, `created_at`) VALUES
-(3, 'W8Y2h4ZP', 'https://imet.metropolitan.ac.rs/student/#/home', '2023-10-10'),
-(4, 'm4M1SDuT', 'https://www.google.com/', '2023-10-10'),
-(5, 'aPB5XtFQ', 'https://getbootstrap.com/', '2023-10-10'),
-(6, 'W7Zfgz8d', 'https://www.instagram.com/', '2023-10-10'),
-(7, '35K0OrD4', 'https://www.instagram.com/', '2023-10-10'),
-(8, '884V15h9', 'https://www.instagram.com/', '2023-10-10'),
-(9, 'wevekXm5', 'https://www.instagram.com/', '2023-10-10'),
-(10, 'KRkkpDu9', 'http://localhost/url_shortener/::1//url_shortener/index.php/?code=aPB5XtFQ', '2023-10-10'),
-(11, '0aJm4Acr', 'https://mail.google.com/mail/u/3/#inbox', '2023-10-10'),
-(12, 'dCyYlz3f', 'https://www.metropolitan.ac.rs/met-ileda-data-viz-hackathon/', '2023-10-10'),
-(13, 'FgPHXhDH', 'https://www.instagram.com/', '2023-10-10'),
-(14, '5gFdZTrC', 'https://www.instagram.com/', '2023-10-10'),
-(15, 'P9JLFcSu', 'https://create-react-app.dev/', '2023-10-10'),
-(16, 'STYQTAMC', 'https://create-react-app.dev/', '2023-10-10'),
-(17, '7hhRjQbd', 'https://www.instagram.com/', '2023-10-10'),
-(18, 'xBgsjim5', 'https://www.instagram.com/', '2023-10-10');
-
---
 -- Indexes for dumped tables
 --
 
